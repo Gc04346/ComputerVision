07d9852a30e14b4f56d9b3eae2c648c4e6a0fbd4
daniel <daniel.santana@onimusic.com.br>
2021-03-30 16:07:31 -0300

    getting a small image easy to count regions by eye to write the algorithm and test it on

--- PS03/main.py
+++ PS03/main.py
@@ -12,5 +12,7 @@ parser.add_argument("-f", "--file", help="Path to the input image")
 # Read arguments from command line
 args = parser.parse_args()
 
-f_one = FormulationOne(args.file, 130)
+# f_one = FormulationOne(args.file, 130)
+f_one = FormulationOne('../imgs/good_for_binarization/chrome.png', 210)
+# f_one.show_binary_image()
 f_one.execute()
