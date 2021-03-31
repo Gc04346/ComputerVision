594eb8239ae5cd2ec60b100219fdcace82950dde
daniel <daniel.santana@onimusic.com.br>
2021-03-30 21:06:30 -0300

    starting algorithms for pathing
    created a recursive algorithm and starting development of a non-recursi
    ve one

--- PS03/formulation_one.py
+++ PS03/formulation_one.py
@@ -1,6 +1,8 @@
+from typing import List, Tuple, Set
+
 import numpy as np
 
-from utils import Utils, G_MAX, GRAYSCALE_IMAGE
+from utils import Utils, G_MAX, GRAYSCALE_IMAGE, BLACK, WHITE
 import cv2 as cv
 
 
@@ -26,19 +28,12 @@ class FormulationOne:
 
     def execute(self):
         while True:
-            input_option = input('Select one of the following:\n(1) Counting Components;\n(2) Geometric Features of a Selected Component;\n(3) Exit.\nSelected option: ')
+            # input_option = input('Select one of the following:\n(1) Counting Components;\n(2) Geometric Features of a Selected Component;\n(3) Exit.\nSelected option: ')
+            input_option = str(1)
             if input_option == str(1):
-                (ret, cnts) = cv.findContours(self.binary_image.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-                idx = 0
-                for c in cnts:
-                    x, y, w, h = cv.boundingRect(c)
-                    if w > 50 and h > 50:
-                        idx += 1
-                        new_img = self.binary_image[y:y + h, x:x + w]
-                        cv.imwrite(str(idx) + '.png', new_img)
-                cv.imshow("im", new_img)
-                cv.waitKey(0)
-                cv.destroyAllWindows()
+                # recursive_adjacency = input('Now, please select (4), for black<white or (8), for white<black.\nSelected option: ')
+                adjacency = 4
+                self.count_components(adjacency)
             elif input_option == str(2):
                 print('you are so beautiful')
             elif input_option == str(3):
@@ -46,6 +41,24 @@ class FormulationOne:
                 break
             else:
                 print('\nInvalid option!\n')
+            break
+
+    def count_components(self, adjacency):
+        visited_pixels: Set[Tuple[int, int]] = set()
+        number_of_regions: int = 0
+        img = self.binary_image
+        shape = img.shape
+        print(f'Starting counting algorithm. We will visit {shape[0]*shape[1]} pixels.')
+        for x, values in enumerate(img):
+            for y, value in enumerate(values):
+                if (x, y) in visited_pixels:
+                    continue
+                # Utils.recursive_adjacency(img, (x, y), visited_pixels, int(adjacency))
+                Utils.regular_adjacency(img, (x, y), visited_pixels, int(adjacency))
+                number_of_regions += 1
+
+        print(f'We have {number_of_regions} black regions.')
+        print(f'The algorithm has visited {len(visited_pixels)} pixels in total.')
 
     def show_binary_image(self):
         cv.imshow(f'Resulting image for threshold T={self.t}', self.binary_image)
--- PS03/utils.py
+++ PS03/utils.py
@@ -10,7 +10,10 @@ import numpy as np
 
 G_MAX = 255
 GRAYSCALE_IMAGE = 0
-
+BLACK = 0
+WHITE = 255
+FOUR_ADJACENCY = 4
+EIGHT_ADJACENCY = 8
 
 class Utils:
 
@@ -34,7 +37,7 @@ class Utils:
         return cv.imread(f'{filepath}/{images[random_index]}', grayscale)
 
     @staticmethod
-    def thresholding_operation(image, threshold=None) -> Tuple[int, int]:
+    def thresholding_operation(image, threshold=None):
         """
         Returns the input image binarized, using a simple threshold algorithm.
         :param image: Input image.
@@ -47,8 +50,66 @@ class Utils:
 
         for x, values in enumerate(image):
             for y, value in enumerate(values):
-                img_j[x][y] = G_MAX if image[x][y] < threshold else 0
+                img_j[x][y] = WHITE if image[x][y] < threshold else BLACK
         return threshold, img_j
+
+    @staticmethod
+    def recursive_adjacency(img, pixel: tuple, visited_pixels: set, adjacency):
+        """
+
+        :param img: input image
+        :param pixel: tuple of coords (x, y)
+        :param visited_pixels: list of visited pixels
+        :param adjacency: indicates whether it is a 4 or 8 adjacency
+        :return:
+        """
+        x, y = pixel
+        eight_adjacency = [(x + 1, y), (x + 1, y - 1), (x, y - 1), (x - 1, y - 1), (x - 1, y), (x - 1, y + 1),
+                           (x, y + 1), (x + 1, y + 1)]
+        four_adjacency = [(x + 1, y), (x, y - 1), (x - 1, y), (x, y + 1)]
+        if adjacency == FOUR_ADJACENCY:
+            for x, y in four_adjacency:
+                if (x, y) in visited_pixels:
+                    continue
+                try:
+                    if img[x][y] == BLACK:
+                        visited_pixels.add(pixel)
+                        print(f'We have visited {len(visited_pixels)} so far.')
+                        Utils.recursive_adjacency(img, (x, y), visited_pixels, adjacency)
+                except IndexError:
+                    pass
+        else:
+            for x, y in eight_adjacency:
+                try:
+                    if img[x][y] == BLACK:
+                        visited_pixels.add(pixel)
+                        print(f'We have visited {len(visited_pixels)} so far.')
+                        Utils.recursive_adjacency(img, (x, y), visited_pixels, adjacency)
+                except IndexError:
+                    pass
+
+    @staticmethod
+    def regular_adjacency(img, pixel, visited_pixels: set, adjacency):
+        x, y = pixel
+        eight_adjacency = [(x + 1, y), (x + 1, y - 1), (x, y - 1), (x - 1, y - 1), (x - 1, y), (x - 1, y + 1),
+                           (x, y + 1), (x + 1, y + 1)]
+        four_adjacency = [(x + 1, y), (x, y - 1), (x - 1, y), (x, y + 1)]
+        if (x, y) in visited_pixels:
+            print(f'Pixel {(x,y)} already visited.')
+            return
+        if adjacency == FOUR_ADJACENCY:
+            for posx, posy in four_adjacency:
+                try:
+                    if (posx, posy) not in visited_pixels:
+                        visited_pixels.add((posx, posy))
+                    if (
+                        img[posx][posy] == BLACK
+                        and (posx, posy) not in visited_pixels
+                    ):
+                        visited_pixels.add((posx, posy))
+                        print(f'We have visited {len(visited_pixels)} so far.')
+                except IndexError:
+                    pass
 
     @staticmethod
     def sliding_window(image, step, window_size):
